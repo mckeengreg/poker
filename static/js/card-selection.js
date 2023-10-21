f118f30ae2e716b5b196f1235e7f7713fa44ee45
Greg McKeen <mckeen.greg@gmail.com>
2023-10-21 10:42:34 +0200

    Adapt the JavaScript to work with multiple card containers

--- static/js/card-selection.js
+++ static/js/card-selection.js
@@ -3,38 +3,40 @@ initializeActiveCards();
 
 
 // Get the cards container
-const cardsContainer = document.querySelector('.cards');
+const cardsContainer = document.querySelectorAll('.cards');
 
 // Add an event listener to the cards container
-cardsContainer.addEventListener('click', function(event) {
-    let cardElement = null;
+cardsContainer.forEach(container => {
+	container.addEventListener('click', function(event) {
+	    let cardElement = null;
 
-    // Check if a card or its children were clicked
-    if (event.target.classList.contains('card')) {
-        cardElement = event.target;
-    } 
-    else if (event.target.closest('.card')) {
-        cardElement = event.target.closest('.card');
-    }
+	    // Check if a card or its children were clicked
+	    if (event.target.classList.contains('card')) {
+	        cardElement = event.target;
+	    } 
+	    else if (event.target.closest('.card')) {
+	        cardElement = event.target.closest('.card');
+	    }
 
-    if (cardElement) {
-        // Using the ID of the cardElement to get the associated checkbox
-        const checkboxName = cardElement.id;
-        const checkbox = document.querySelector(`input[data-name="${checkboxName}"]`);
-        
-        // Toggle the checkbox's checked state
-        checkbox.checked = !checkbox.checked;
+	    if (cardElement) {
+	        // Using the ID of the cardElement to get the associated checkbox
+	        const checkboxName = cardElement.id;
+	        const checkbox = document.querySelector(`input[data-name="${checkboxName}"]`);
+	        
+	        // Toggle the checkbox's checked state
+	        checkbox.checked = !checkbox.checked;
 
-        // Add or remove the "active" class based on the checkbox's state
-        if (checkbox.checked) {
-            cardElement.classList.add('active');
-        } else {
-            cardElement.classList.remove('active');
-        }
+	        // Add or remove the "active" class based on the checkbox's state
+	        if (checkbox.checked) {
+	            cardElement.classList.add('active');
+	        } else {
+	            cardElement.classList.remove('active');
+	        }
 
-        // Use AJAX to submit the form
-        submitFormAjax();
-    }
+	        // Use AJAX to submit the form
+	        submitFormAjax();
+	    }
+	});
 });
 
 // This function adds the 'active' class to cards with checkboxes that are already checked
